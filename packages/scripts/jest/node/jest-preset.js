0f47ea7bb70864ea79dc0a596c0ec48d1b852148
nobu1308 <124151829+nobu1308@users.noreply.github.com>
2023-02-03 23:03:36 +0900

    Make directory structure flatten

--- packages/scripts/jest/node/jest-preset.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  roots: ['<rootDir>'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  modulePathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/dist'],
-  preset: 'ts-jest'
-};
